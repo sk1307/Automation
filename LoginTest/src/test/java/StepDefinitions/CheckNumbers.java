ca3f469b2d8ba5064e4bde52f936ce44c1ee6319
sk1307 <stalishetti@usf.edu>
2024-12-31 13:49:05 -0500

    Adding a new file

--- LoginTest/src/test/java/StepDefinitions/CheckNumbers.java
+++ LoginTest/src/test/java/StepDefinitions/CheckNumbers.java
@@ -1,12 +1,20 @@
 package StepDefinitions;
 
-import org.junit.Test;
-
 public class CheckNumbers {
 	
-	@Test
-	public void CheckNum() {
-		System.out.println(2);
+	public static void main(String []args) {
+	
+		for(int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+		
+		
 	}
+	
+//	cd /Users/sravankumartalishetti/git/Automation 
+//	git init
+//	git add .
+//	git push -u origin main 
+//	git commit -m "Adding a new file"
 
 }
